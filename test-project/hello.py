8bebe47f62feec955b6ed38002643a0add243917
fabulousmistered <fabulousmistered@gmail.com>
2024-10-15 12:19:25 +0000

    second

--- test-project/hello.py
+++ test-project/hello.py
@@ -1,21 +1,5 @@
 from cltk import NLP
 
-# Initialize the NLP model for Latin language
+vitruvius = "Architecti est scientia pluribus disciplinis et variis eruditionibus ornata, quae ab ceteris artibus perficiuntur. Opera ea nascitur et fabrica et ratiocinatione."
+
 cltk_nlp = NLP(language="lat")
-
-# Input text in Latin
-text = (
-    "In nova fert animus mūtātās dīcere fōrmās corpora; dī, coeptīs (nam vōs mūtāstis et illās) "
-    "adspīrāte meīs prīmāque ab orīgine mundī ad mea perpetuum dēdūcite tempora carmen! "
-    "Ante mare et terrās et quod tegit omnia caelum ūnus erat tōtō nātūrae vultus in orbe, "
-    "quem dīxēre chaos rudis indīgestaque mōlēs nec quicquam nisi pondus iners congestaque eōdem "
-    "nōn bene iūnctārum discordia sēmina rērum."
-)
-
-# Analyze the text
-cltk_doc = cltk_nlp.analyze(text=text)
-
-# Print the first 12 tokens of the analyzed document
-print(cltk_doc.tokens[:12])
-cltk_nlp.analyze(text=text)
-print (cltk_doc.tokens[:12])
