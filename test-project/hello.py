1a2367490d68085ab5c2903b7f7b84aa7115ff46
fabulousmistered <fabulousmistered@gmail.com>
2024-10-15 12:36:13 +0000

    4th

--- test-project/hello.py
+++ test-project/hello.py
@@ -5,3 +5,9 @@ vitruvius = "Architecti est scientia pluribus disciplinis et variis eruditionibu
 cltk_nlp = NLP(language="lat")
 
 cltk_doc = cltk_nlp.analyze(text=vitruvius)
+
+cltk_doc.tokens[:5]
+cltk_doc.lemmata[:5]
+cltk_doc.morphosyntactic_features[2]  # 'scientia'
+cltk_doc.pos[:5]
+cltk_doc.sentences_tokens
