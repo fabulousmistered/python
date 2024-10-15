c076f3d5ca0af933048e040db5f56c870500c253
fabulousmistered <fabulousmistered@gmail.com>
2024-10-15 12:20:38 +0000

    3rd

--- test-project/hello.py
+++ test-project/hello.py
@@ -3,3 +3,5 @@ from cltk import NLP
 vitruvius = "Architecti est scientia pluribus disciplinis et variis eruditionibus ornata, quae ab ceteris artibus perficiuntur. Opera ea nascitur et fabrica et ratiocinatione."
 
 cltk_nlp = NLP(language="lat")
+
+cltk_doc = cltk_nlp.analyze(text=vitruvius)
