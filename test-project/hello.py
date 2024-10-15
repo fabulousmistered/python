f1cd43d97f9e4df87df7901f82bf912d765d578a
fabulousmistered <fabulousmistered@gmail.com>
2024-10-15 12:13:02 +0000

    commit

--- test-project/hello.py
+++ test-project/hello.py
@@ -1,6 +1,21 @@
-#-------------------------------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
-#-------------------------------------------------------------------------------------------------------------
+from cltk import NLP
 
-print('Hello, remote world!')
+# Initialize the NLP model for Latin language
+cltk_nlp = NLP(language="lat")
+
+# Input text in Latin
+text = (
+    "In nova fert animus mūtātās dīcere fōrmās corpora; dī, coeptīs (nam vōs mūtāstis et illās) "
+    "adspīrāte meīs prīmāque ab orīgine mundī ad mea perpetuum dēdūcite tempora carmen! "
+    "Ante mare et terrās et quod tegit omnia caelum ūnus erat tōtō nātūrae vultus in orbe, "
+    "quem dīxēre chaos rudis indīgestaque mōlēs nec quicquam nisi pondus iners congestaque eōdem "
+    "nōn bene iūnctārum discordia sēmina rērum."
+)
+
+# Analyze the text
+cltk_doc = cltk_nlp.analyze(text=text)
+
+# Print the first 12 tokens of the analyzed document
+print(cltk_doc.tokens[:12])
+cltk_nlp.analyze(text=text)
+print (cltk_doc.tokens[:12])
